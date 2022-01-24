dd274b19fdb1235846b97180b515d1538d671931
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-24 11:13:19 +0530

    added removing Digits

---
+++ removingDigits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getMax(int n){
+    string s = to_string(n);
+    int mx = -1;
+    for(int i = 0;i<s.size();i++){
+        mx = max(mx, stoi(s[i] + string()));
+    }
+    return mx;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int cnt = 0 ;
+    while(n > 0){
+        int mx = getMax(n);
+        n -= mx;
+        cnt++;
+    }
+    cout <<cnt<<endl;
+    return 0;
+}
