eb1c53c34e12169a5a7d8f344b1265e8834003a1
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-25 01:08:05 +0530

    all the answers are correct but there is runtime error in elevator rides

--- elevatorRides.cpp
+++ elevatorRides.cpp
@@ -11,11 +11,22 @@ int main(){
     int v = -1;
     int x;
     int cnt=  0;
+    if(n == 1){  
+        cout <<1<<endl;
+        return 0;
+    }
     while(!a.empty()){
-        if(v == -1) v = a.back();
+        // cout <<v<<endl;
+        if(v == -1){
+            v = a.back();
+        }
         x = k - v;
         auto ptr = lower_bound(a.begin(), a.end(), x);
-        if(*ptr == x && (ptr - a.begin() != a.size() - 1)){
+        if(ptr == a.end()){
+            ptr--;
+            ptr--;
+        }
+        if(*ptr == x){
             a.erase(ptr);
             a.pop_back();
             v = -1;
@@ -33,6 +44,8 @@ int main(){
                 a.erase(ptr);
             }
         }
+        // for(int i : a)   cout <<i<<' ';
+        // cout <<endl;
     }
     cout <<cnt<<endl;
     return 0;
