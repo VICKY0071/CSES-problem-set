4e17021fb8d51d4b2f1e8dded3890013a46c6787
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-25 00:34:12 +0530

    made my first attempt at elevator rides but got 2 WA and 3-4 RE

---
+++ elevatorRides.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0;i<n;i++)  cin >> a[i];
+    sort(a.begin(), a.end());
+    int v = -1;
+    int x;
+    int cnt=  0;
+    while(!a.empty()){
+        if(v == -1) v = a.back();
+        x = k - v;
+        auto ptr = lower_bound(a.begin(), a.end(), x);
+        if(*ptr == x && (ptr - a.begin() != a.size() - 1)){
+            a.erase(ptr);
+            a.pop_back();
+            v = -1;
+            cnt++;
+        }
+        else{
+            if(ptr == a.begin()){
+                a.pop_back();
+                v = -1;
+                cnt++;
+            }
+            else{
+                ptr--;
+                v += *ptr;
+                a.erase(ptr);
+            }
+        }
+    }
+    cout <<cnt<<endl;
+    return 0;
+}
