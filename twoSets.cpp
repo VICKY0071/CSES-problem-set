b4146d179d90c93c5c9151ef0e962f6109d80b48
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-24 12:00:55 +0530

    otimized the twoset solution

--- twoSets.cpp
+++ twoSets.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-
+#define int long long
 using namespace std;
 
-int main(){
+int32_t main(){
     int n;
     cin >> n;
     int sum = (n * (n + 1)) / 2;
@@ -10,7 +10,27 @@ int main(){
         cout<<"NO\n";
         return 0;
     }
-
+    vector<int> res, res2;
+    int hlf = (sum >> 1);
+    for(;n >= 1;n--){
+        if(hlf > 0 && n <= hlf){
+            res.push_back(n);
+            hlf -= n;
+        }
+        else{
+            res2.push_back(n);
+        }
+    }
+    if(hlf == 0){
+        cout <<"YES\n";
+        cout <<res.size()<<endl;
+        for(int i : res)    cout <<i<<' ';
+        cout <<endl;
+        cout <<res2.size()<<endl;
+        for(int i : res2)   cout <<i<<' ';
+        cout <<endl;
+    }
+    else    cout <<"NO\n";
     return 0;
 
 }
