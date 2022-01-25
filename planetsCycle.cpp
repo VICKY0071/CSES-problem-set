e80a986c8274a44041f899554fad51393b859139
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-26 02:48:39 +0530

    added teh solution to the planets cycle optimized and complete AC

--- planetsCycle.cpp
+++ planetsCycle.cpp
@@ -2,8 +2,8 @@
 #define int long long
 using namespace std;
 
-int colors[2000001];
-int head[2000001];
+int colors[200001];
+int head[200001];
 
 vector<int> order;
 void dfs1(vector<vector<int>>& a, int i){
@@ -28,76 +28,58 @@ void dfs2(vector<vector<int>>& a, int i, int rep){
 
 int v, d;
 void dfs(unordered_map<int, int>& cnt, vector<vector<int>>& a, int src){
-    // cout <<src<<endl;
-    if(cnt[src] != 1 || (cnt[src] == 1 && a[src][0] == src)){
-        v = cnt[src];
+    if(cnt[head[src]] != 1 || (cnt[head[src]] == 1 && a[src][0] == src)){
+        v = cnt[head[src]];
         return;
     }
-    // cout <<src<<endl;
     dfs(cnt, a, a[src][0]);
     d++;
-    cnt[src] = d + v;
+    cnt[head[src]] = d + v;
 }
 
 int32_t main(){
     int n;
     cin >> n;
     memset(head, -1, sizeof head);
+    memset(colors, 0, sizeof colors);
     vector<vector<int>> a(n);
+    vector<vector<int>> rev(n);
     for(int i = 0;i<n;i++){
         int v;
         cin >> v;
         v -= 1;
         a[i].push_back(v);
+        rev[v].push_back(i);
     }
     for(int i = 0;i<n;i++){
         if(colors[i] == 0){
             dfs1(a, i);
         }
     }
-    vector<vector<int>> rev(n);
-    for(int i = 0;i<n;i++){
-        for(int j : a[i]){
-            rev[j].push_back(i);
+
+    for(int i = order.size() - 1;i >= 0;i--){
+        if(colors[order[i]] == 1){
+            dfs2(rev, order[i], order[i]);
         }
     }
-    for(int i = order.size();i >= 0;i--){
-        if(colors[i] == 1){
-            dfs2(rev, i, i);
-        }
-    }
-    // for(int i = 0;i<n;i++)  cout <<head[i]<<' ';
-    // cout <<endl;
-    unordered_map<int, int> cnt;
-    for(int i = 0;i<n;i++){
-        cnt[head[i]]++;
-    }
-    for(int i = 0;i<n;i++){
-        cnt[i] = cnt[head[i]];
-    }
-    // for(auto i : cnt){
-    //     cout <<i.first<<"->"<<i.second<<endl;
-    // }
     vector<int> ans(n);
+    unordered_map<int, int> cnt;
+    for(int i = 0;i<n;i++)  cnt[head[i]]++;
     for(int i = 0;i<n;i++){
-        if(cnt[i] == 1){
-            if(a[i][0] == i){
+        if(cnt[head[i]] == 1){
+            if(a[i][0] == i ){
                 ans[i] = 1;
             }
             else{
+                d=  0;
                 dfs(cnt, a, i);
-                ans[i] = cnt[i];
+                ans[i] = cnt[head[i]];
             }
         }
         else{
             ans[i] = cnt[head[i]];
         }
     }
-    // for(auto i : cnt){
-    //     cout <<i.first<<"->"<<i.second<<endl;
-    // }
-    for(int i : ans){
-        cout<<i<<' ';
-    }
+    for(int i : ans)    cout <<i<<' ';
     return 0;
 }
