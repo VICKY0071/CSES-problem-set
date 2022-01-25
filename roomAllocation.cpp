8cee0b1af00fa092de173d594e007ecd53bea903
Vikas Thapliyal <vikasthapliyal531@gmail.com>
2022-01-25 21:40:12 +0530

    optimized the room allocation question

--- roomAllocation.cpp
+++ roomAllocation.cpp
@@ -15,18 +15,23 @@ int32_t main(){
     int room = 1;
     vector<int> ans(n);
     ans[a[0][2]] = 1;
-    rooms.push_back(a[0][1]);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    q.push({a[0][1], 1});
+    int id = 1;
     for(int i = 1;i<n;i++){
         int j = 0;
         for(;j < rooms.size() && rooms[j] >= a[i][0];j++);
-        if(j == rooms.size()){
-            room++;
-            rooms.push_back(a[i][1]);
-        }
+        if(!q.empty() && q.top().first < a[i][0]){
+            int rm = q.top().second;
+            q.pop();
+            ans[a[i][2]] = rm;
+            q.push({a[i][1], rm});
+        }   
         else{
-            rooms[j] = a[i][1];
+            room++;
+            q.push({a[i][1], ++id});
+            ans[a[i][2]] = id;
         }
-        ans[a[i][2]] = j + 1;
     }
     cout <<room<<endl;
     for(int i : ans){
